a563e8f7fdf1987643a2c52366e3409b7713d9aa
Ayush Dhyani <ayushdhyani764@gmail.com>
2026-06-11 22:55:07 +0530

    add prettier settings

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
-  
+
+import connectDB from "./db/index.js";
+import dotenv from "dotenv"
+
+dotenv.config({
+    path:'./env'
+})
+
+connectDB()
+
+
+
+// import e from "express";
+// const app=express();
+// (async ()=>{
+//     try{
+//        await mongoose.connect(`${process.env.MONGODB_URL}/
+//         ${DB_NAME}`)
+//     }catch(error){
+//         console.error("error",error);
+//         throw err
+//     }
+// })()
